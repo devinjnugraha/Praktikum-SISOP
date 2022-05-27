c303050d589944d7b5786254d61632ca9d8c1abe
Devin Nugraha <79150730+devinjnugraha@users.noreply.github.com>
2022-05-27 15:04:06 +0700

    Update move.c

--- 9 Input Output/move.c
+++ 9 Input Output/move.c
@@ -5,8 +5,6 @@
 extern int errno;
 
 int main(int argc, char* argv[]){
-	// if file does not have in directory
-	// then file foo.txt is created.
 	int fd = open(argv[1], O_RDWR);
 	printf("fd = %d\n", fd);
 	if (fd != -1){
