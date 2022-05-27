991a9ea1facbad6325e37afa92cfa07d041ad0ab
Devin Nugraha <79150730+devinjnugraha@users.noreply.github.com>
2022-05-27 15:03:45 +0700

    Update and rename copy.c to copy300.c

--- 9 Input Output/copy300.c
+++ 9 Input Output/copy300.c
@@ -7,12 +7,12 @@ extern int errno;
 int main(int argc, char* argv[]){
 	int fd = open(argv[1], O_RDWR);
 	printf("fd = %d\n", fd);
-	char temp[100];
+	char temp[300];
 	if (fd != -1){
-		read(fd, temp, 50);
+		read(fd, temp, sizeof(temp));
 		close(fd);
 		int fd2 = open(argv[2], O_RDWR | O_CREAT);
-		write(fd2, temp, 50);
+		write(fd2, temp, sizeof(temp));
 		close(fd2);
 	}
 	return 0;
