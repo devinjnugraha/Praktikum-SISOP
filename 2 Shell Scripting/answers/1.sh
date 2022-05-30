8f077685c172151779f0a625d0d2f5339c417c92
devinjnugraha <devinjn123@gmail.com>
2022-05-31 00:13:29 +0700

    modified 2

--- 2 Shell Scripting/answers/1.sh
+++ 2 Shell Scripting/answers/1.sh
@@ -1,3 +1,3 @@
 echo 'Single Quote ini dengan adanya "Double Quote" di antaranya';
-echo "Double quote dengan \”double quote\” kah?";
+echo "Double quote dengan \"double quote\" kah?";
 echo "isi direktori `pwd` adalah `ls|wc -l` file"
